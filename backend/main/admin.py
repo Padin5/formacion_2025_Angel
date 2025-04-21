ed991c51e2ead59a16ca143bd8dced24dca7e455
Angel.Alvarez.Padin.5 <126275804+Padin5@users.noreply.github.com>
2025-04-21 18:21:06 +0200

    Vista y controllers

--- backend/main/admin.py
+++ backend/main/admin.py
@@ -2,5 +2,27 @@ from django.contrib import admin
 
 from main.models import Expense
 
+## Podemos modificar la interfaz del admin de nuestro localhost
+class ExpenseAdmin(admin.ModelAdmin):
+    ## Los fields son cabeceros de columna, para que no aparezca todo apelmazado
+    fields = (
+        "descripcion",
+        "categoria",
+        "limite",
+    )
+    ## Lo que nos aparece una vez introducimos una fila
+    list_display = (
+        "descripcion",
+        "categoria",
+        "limite",
+        "fecha",
+    )
+    ## Filtrar por categoría
+    list_filter = ("categoria", "limite")
+    ## Buscador
+    search_fields = ("categoria")
+    ## Para ordenarlos
+    ordering = ("limite")
+
 admin.site.register(Expense)
 
--- backend/main/models.py
+++ backend/main/models.py
@@ -1,13 +1,16 @@
 from django.db import models
 
+## Importamos una funcion de traduccion
+from django.utils.translation import gettext_lazy as _
+
 ## Los modelos son básicamente Clases, dentro de la programación orientada a objetos
 ## Importamos la superclase 'models' de Django
 class Expense(models.Model):
 
 ## Definimos los atributos de clase
     ## Charfield por ejemplo sería el tipo char (String) y tenemos que definir su longitud, hay muchos más parámetros a definir
-    descripcion = models.CharField(max_length=255)
-    limite = models.DecimalField(max_digits=10, decimal_places=2)
+    descripcion = models.CharField(max_length=255, verbose_name=_("Description"))
+    limite = models.DecimalField(max_digits=10, decimal_places=2, verbose_name=_("Limit"))
     fecha = models.DateTimeField(auto_created=True)
 
     ## Los choices son Arrays y funcionan como tuplas, solo nos dejará introducir los caracteres definidos
@@ -17,7 +20,7 @@ class Expense(models.Model):
         ('ENTR', 'Entertainment'),
         ('UTIL', 'Utilities'),
         ('OTHR', 'Other')
-    ])
+    ], verbose_name=_("Category"))
 
     def __str__(self):
         return f"{self.descripcion} - {self.limite} - {self.fecha} - {self.categoria}"
